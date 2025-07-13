8ebc18cd6dbc8b834292e4aef25a401458cc94a1
Chathura Samarajeewa <chathura.samarajeewa@gmail.com>
2025-07-13 11:49:13 +0530

    fix: adds login feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-// App.js
-
 import { useAuth } from "react-oidc-context";
 
 function App() {
   const auth = useAuth();
+  const [todos, setTodos] = useState([]);
+  const [apiInProgress, setApiInProgress] = useState(true);
 
   const signOutRedirect = () => {
     const clientId = "6tb1dlmmp6p58coevc310ovl4i";
@@ -23,15 +23,105 @@ function App() {
     return <div>Encountering error... {auth.error.message}</div>;
   }
 
+  useEffect(() => {
+    setApiInProgress(true);
+    fetch(`${API_URL}/todo`)
+      .then((response) => response.json())
+      .then((data) => setTodos(data))
+      .catch((error) => console.error(error))
+      .finally(() => setApiInProgress(false));
+  }, []);
+
+  const addTodo = (newTodo) => {
+    setTodos((prevTodos) => [newTodo, ...prevTodos]);
+  };
+
+  const toggleComplete = async (id) => {
+    const todoToUpdate = todos.find((todo) => todo.todoId === id);
+    if (!todoToUpdate) return;
+
+    // Toggle status
+    const newStatus =
+      todoToUpdate.todoStatus === "COMPLETED" ? "PENDING" : "COMPLETED";
+
+    // Send PATCH request to backend
+    try {
+      setApiInProgress(true);
+      const response = await fetch(`${API_URL}/todo`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: todoToUpdate.userId,
+          todoId: todoToUpdate.todoId,
+          status: newStatus.toLowerCase(),
+        }),
+      });
+      setApiInProgress(false);
+
+      if (response.ok) {
+        setTodos(
+          todos.map((todo) =>
+            todo.todoId === id ? { ...todo, todoStatus: newStatus } : todo
+          )
+        );
+      } else {
+        console.error("Failed to update todo status");
+      }
+    } catch (error) {
+      console.error("Error updating todo status:", error);
+    }
+  };
+
+  const deleteTodo = async (id) => {
+    const todoToDelete = todos.find((todo) => todo.todoId === id);
+    if (!todoToDelete) return;
+
+    try {
+      setApiInProgress(true);
+      const response = await fetch(`${API_URL}/todo`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: "chathuras940@gmail.com",
+          todoId: id,
+        }),
+      });
+      setApiInProgress(false);
+
+      if (response.ok) {
+        setTodos(todos.filter((todo) => todo.todoId !== id));
+      } else {
+        console.error("Failed to delete todo");
+      }
+    } catch (error) {
+      console.error("Error deleting todo:", error);
+    }
+  };
+
   if (auth.isAuthenticated) {
     return (
-      <div>
+      <div className="todo-container">
         <pre> Hello: {auth.user?.profile.email} </pre>
         <pre> ID Token: {auth.user?.id_token} </pre>
         <pre> Access Token: {auth.user?.access_token} </pre>
         <pre> Refresh Token: {auth.user?.refresh_token} </pre>
 
         <button onClick={() => auth.removeUser()}>Sign out</button>
+
+        <TopMenu />
+        <h1>AWS Powered Todo App.</h1>
+        {console.log(apiInProgress)}
+        {apiInProgress && <ProgressCircle />}
+        <TodoInput onAdd={addTodo} />
+        <TodoItemList
+          todos={todos}
+          handleToggle={toggleComplete}
+          handleDelete={deleteTodo}
+        />
       </div>
     );
   }
